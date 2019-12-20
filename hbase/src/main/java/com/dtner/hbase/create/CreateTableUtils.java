3a61e772788c88be19ea96ea91c3f29d897264ac
dt <245548212@qq.com>
2019-12-20 15:04:48 +0800

    提交 Hbase 插入

---
+++ hbase/src/main/java/com/dtner/hbase/create/CreateTableUtils.java
@@ -0,0 +1,35 @@
+package com.dtner.hbase.create;
+
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.*;
+
+import java.io.IOException;
+import java.util.Collection;
+
+/**
+ * @ClassName CreateTable
+ * @Description: 创建 Hbase 表
+ * @Author dt
+ * @Date 19-12-20
+ **/
+public class CreateTableUtils {
+
+    /**
+     * 创建 Hbase 表结构
+     * @param con
+     * @param tableName
+     * @param familyDescriptors
+     * @throws IOException
+     */
+    public static void CreateTable(Connection con, TableName tableName, Collection<ColumnFamilyDescriptor> familyDescriptors) throws IOException {
+        Admin admin = con.getAdmin();
+        /*表如果不存在，则创建表*/
+        if(!admin.tableExists(tableName)){
+            TableDescriptor tableDescriptor = TableDescriptorBuilder.newBuilder(tableName)
+                    .setColumnFamilies(familyDescriptors).build();
+            admin.createTable(tableDescriptor);
+        }
+        admin.close();
+    }
+
+}
--- hbase/src/main/java/com/dtner/hbase/crud/CreateData.java
+++ hbase/src/main/java/com/dtner/hbase/crud/CreateData.java
@@ -1,15 +1,16 @@
 package com.dtner.hbase.crud;
 
+import com.dtner.hbase.create.CreateTableUtils;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.HBaseConfiguration;
-import org.apache.hadoop.hbase.HTableDescriptor;
 import org.apache.hadoop.hbase.TableName;
 import org.apache.hadoop.hbase.client.*;
 import org.apache.hadoop.hbase.util.Bytes;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @Author: dt
@@ -24,6 +25,10 @@ public class CreateData {
 
     }
 
+    /**
+     * 普通方式保存
+     * @throws IOException
+     */
     @Test
     public void testPut() throws IOException {
 
@@ -33,29 +38,126 @@ public class CreateData {
 
         TableName tableName = TableName.valueOf("user_test");
 
-        /*表如果不存在，则创建表*/
-        if(!admin.tableExists(tableName)){
-            TableDescriptor tableDescriptor = TableDescriptorBuilder.newBuilder(tableName).build();
-            admin.createTable(tableDescriptor);
-        }
+        // Column Family
+       String firstFamily = "dtner-family-1";
+       String secondFaily = "dtner-family-2";
 
+        /*表如果不存在，则创建表*/
+        List<ColumnFamilyDescriptor> familys = new ArrayList<>();
+        familys.add(ColumnFamilyDescriptorBuilder.of(firstFamily));
+        familys.add(ColumnFamilyDescriptorBuilder.of(secondFaily));
+        CreateTableUtils.CreateTable(con,tableName,familys);
 
         Table table = con.getTable(tableName);
 
         // rowkey
         String rowkey = "ths test rowkey"+System.currentTimeMillis();
-
+        // 保存数据
         Put put = new Put(Bytes.toBytes(rowkey));
-        // Column Family
-        String familyCol = "coldam1";
-        put.addColumn(Bytes.toBytes(familyCol),Bytes.toBytes("test1"),Bytes.toBytes(value1));
-        put.addColumn(Bytes.toBytes(familyCol),Bytes.toBytes("test2"),Bytes.toBytes(value2));
-        put.addColumn(Bytes.toBytes(familyCol),Bytes.toBytes("test2"),Bytes.toBytes(value3));
+        put.addColumn(Bytes.toBytes(firstFamily),Bytes.toBytes("test-1-1"),Bytes.toBytes("value-1-1"));
+        put.addColumn(Bytes.toBytes(firstFamily),Bytes.toBytes("test-1-2"),Bytes.toBytes("value-1-2"));
+        put.addColumn(Bytes.toBytes(firstFamily),Bytes.toBytes("test-1-2"),Bytes.toBytes("value-1-3"));
+        put.addColumn(Bytes.toBytes(secondFaily),Bytes.toBytes("test-2-1"),Bytes.toBytes("value-2-1"));
         table.put(put);
         closeCon(con);
 
     }
 
+    /**
+     * 批量插入数据到 hbase
+     * 还可以通过 bath api 来使用
+     * @throws IOException
+     */
+    @Test
+    public void bathPut() throws Exception {
+
+        Connection con = getCon();
+        TableName tableName = TableName.valueOf("user_test_bath");
+        // Column Family
+        String firstFamily = "dtner-family-1";
+        String secondFaily = "dtner-family-2";
+
+        /*列族*/
+        List<ColumnFamilyDescriptor> list = new ArrayList<>();
+        list.add(ColumnFamilyDescriptorBuilder.of(firstFamily));
+        list.add(ColumnFamilyDescriptorBuilder.of(secondFaily));
+        CreateTableUtils.CreateTable(con,tableName,list);
+
+        Table table = con.getTable(tableName);
+
+        List<Put> putList = new ArrayList<>();
+        Put put = new Put(Bytes.toBytes("bath_put_1_"+System.currentTimeMillis()));
+        put.addColumn(Bytes.toBytes(firstFamily),Bytes.toBytes("bath-qualifier-1-1"),Bytes.toBytes("bath-value-1-1"));
+        put.addColumn(Bytes.toBytes(firstFamily),Bytes.toBytes("bath-qualifier-1-2"),Bytes.toBytes("bath-value-1-2"));
+        put.addColumn(Bytes.toBytes(secondFaily),Bytes.toBytes("bath-qualifier-1-3"),Bytes.toBytes("bath-value-1-3"));
+        putList.add(put);
+
+        Put put1 = new Put(Bytes.toBytes("bath_put_2_" + System.currentTimeMillis()));
+        put1.addColumn(Bytes.toBytes(firstFamily),Bytes.toBytes("bath-qualifier-2-1"),Bytes.toBytes("bath-value-2-1"));
+        put1.addColumn(Bytes.toBytes(firstFamily),Bytes.toBytes("bath-qualifier-2-2"),Bytes.toBytes("bath-value-2-2"));
+        put1.addColumn(Bytes.toBytes(secondFaily),Bytes.toBytes("bath-qualifier-2-3"),Bytes.toBytes("bath-value-2-3"));
+        putList.add(put1);
+
+        // 保存集合 put
+//        table.put(putList);
+
+        Object[] results = new Object[putList.size()];
+        table.batch(putList,results);
+
+        System.out.println(results.toString());
+
+        con.close();
+
+    }
+
+
+    /**
+     * cas
+     * @throws Exception
+     */
+    @Test
+    public void casPut() throws Exception {
+
+        Connection con = getCon();
+        TableName tableName = TableName.valueOf("user_test_cas");
+        // Column Family
+        String firstFamily = "dtner-family-1";
+        String secondFaily = "dtner-family-2";
+
+        /*列族*/
+        List<ColumnFamilyDescriptor> list = new ArrayList<>();
+        list.add(ColumnFamilyDescriptorBuilder.of(firstFamily));
+        list.add(ColumnFamilyDescriptorBuilder.of(secondFaily));
+        CreateTableUtils.CreateTable(con,tableName,list);
+
+        Table table = con.getTable(tableName);
+
+        Put put = new Put(Bytes.toBytes("cas_put_1"));
+        put.addColumn(Bytes.toBytes(firstFamily),Bytes.toBytes("cas-qualifier-1-1"),Bytes.toBytes("cas-value-1-1"));
+        put.addColumn(Bytes.toBytes(firstFamily),Bytes.toBytes("cas-qualifier-1-2"),Bytes.toBytes("cas-value-1-2"));
+        put.addColumn(Bytes.toBytes(secondFaily),Bytes.toBytes("cas-qualifier-1-3"),Bytes.toBytes("cas-value-1-3"));
+        table.put(put);
+
+        Put putUpdate = new Put(Bytes.toBytes("cas_put_1"));
+        putUpdate.addColumn(Bytes.toBytes(firstFamily),Bytes.toBytes("cas-qualifier-2-1"),Bytes.toBytes("cas-value-2-1"));
+        putUpdate.addColumn(Bytes.toBytes(firstFamily),Bytes.toBytes("cas-qualifier-2-2"),Bytes.toBytes("cas-value-2-2"));
+        putUpdate.addColumn(Bytes.toBytes(secondFaily),Bytes.toBytes("cas-qualifier-2-3"),Bytes.toBytes("cas-value-2-3"));
+
+        // 如果不存在字段就插入
+        table.checkAndMutate(Bytes.toBytes("cas_put_1"),Bytes.toBytes(firstFamily))
+                .qualifier(Bytes.toBytes("cas-qualifier-1-1"))
+                .ifNotExists()
+                .thenPut(putUpdate);
+
+    }
+
+
+
+    /**
+     * 关闭连接
+     * @param con
+     * @throws IOException
+     */
     public void closeCon(Connection con) throws IOException {
         con.close();
     }
